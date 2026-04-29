7b335b425f21d4580f8e8d816a14e64d036a941c
Antonio Galdo <antoniogs9@hotmail.es>
2026-04-29 16:43:36 +0000

    incrementality and packages

--- models/staging/posgress_db/stg_postgres_addresses.sql
+++ models/staging/posgress_db/stg_postgres_addresses.sql
@@ -1,7 +1,15 @@
-{{ config(materialized='view') }}
+--{{ config(materialized='view') }}
+{{ config(
+    materialized='incremental',
+    unique_key='ADDRESS_ID',
+    incremental_strategy='merge'
+) }}
 
 WITH source AS (
     SELECT * FROM {{ source('postgres_source', 'POSTGRES_ADDRESSES') }}
+{% if is_incremental() %}
+    WHERE _FIVETRAN_SYNCED > (SELECT MAX(LOADED_AT) FROM {{ this }})
+{% endif %}
 ),
 
 renamed AS (
@@ -13,7 +21,8 @@ renamed AS (
         COUNTRY,
         _FIVETRAN_SYNCED                  AS LOADED_AT
     FROM source
-    WHERE COALESCE(DELETED, 'false') != 'true'
+    --WHERE COALESCE(DELETED, 'false') != 'true'
+
 )
 
 SELECT * FROM renamed
