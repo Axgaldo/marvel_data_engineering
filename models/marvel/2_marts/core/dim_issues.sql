e10c79d2d49696ada04cc651ea051b0c0e5a9078
Antonio Galdo <antoniogs9@hotmail.es>
2026-05-17 00:40:57 +0000

    TABLON 2

--- models/marvel/2_marts/core/dim_issues.sql
+++ models/marvel/2_marts/core/dim_issues.sql
@@ -8,14 +8,17 @@ with current_issues as (
 
 ),
 
-issue_multiversal_metrics as (
+issue_character_metrics as (
 
     select
+
         s.issue_id,
 
-        count(distinct sc.character_id) as total_characters_present,
+        count(distinct sc.character_id)
+            as total_characters_present,
 
-        count(distinct c.universe_id) as total_universes_involved
+        count(distinct c.universe_id)
+            as total_universes_involved
 
     from {{ ref('stg_stories') }} s
 
@@ -23,7 +26,25 @@ issue_multiversal_metrics as (
         on s.story_id = sc.story_id
 
     inner join {{ ref('stg_characters') }} c
-        on sc.character_id = c.character_id::string
+        on sc.character_id = c.character_id
+
+    group by 1
+
+),
+
+issue_artist_metrics as (
+
+    select
+
+        s.issue_id,
+
+        count(distinct sa.artist_id)
+            as total_artists
+
+    from {{ ref('stg_stories') }} s
+
+    inner join {{ ref('stg_story_artists') }} sa
+        on s.story_id = sa.story_id
 
     group by 1
 
@@ -32,51 +53,106 @@ issue_multiversal_metrics as (
 select
 
     i.issue_id,
+
     i.issue_title,
+
     i.issue_url,
+
     i.cover_url,
+
     i.description,
 
+    i.rating_avg,
+
+    i.ratings_count,
+
+    round(
+        i.rating_avg
+        *
+        sqrt(
+            i.ratings_count
+            /
+            nullif(
+                max(i.ratings_count) over (),
+                0
+            )
+        ),
+        2
+    ) as issue_weighted_rating,
+
     i.num_user_owns,
+
     i.num_user_reads,
+
     i.num_user_wishes,
 
     i.release_date,
+
     i.cover_date,
 
     i.num_pages,
+
     i.price_dollars,
+
+    (
+        coalesce(i.price_dollars, 0)
+        *
+        coalesce(i.num_user_owns, 0)
+    ) as estimated_market_revenue,
+
+    case
+        when i.num_pages > 0 then
+            round(i.rating_avg / i.num_pages, 4)
+        else 0
+    end as quality_per_page_ratio,
 
     s.series_title,
 
     f.format_name,
 
     i.sku,
+
     i.upc,
 
-    -- MÉTRICAS MULTIVERSALES
+    /* =========================================
+       MULTIVERSAL METRICS
+    ========================================= */
 
-    coalesce(m.total_characters_present, 0)
+    coalesce(cm.total_characters_present, 0)
         as total_characters_present,
 
-    coalesce(m.total_universes_involved, 0)
+    coalesce(cm.total_universes_involved, 0)
         as total_universes_involved,
 
     case
-        when coalesce(m.total_universes_involved, 0) > 1
+        when coalesce(cm.total_universes_involved, 0) > 1
         then true
         else false
     end as is_multiversal_event,
 
     (
-        coalesce(m.total_characters_present, 0)
+        coalesce(cm.total_characters_present, 0)
         *
-        coalesce(m.total_universes_involved, 0)
+        coalesce(cm.total_universes_involved, 0)
     ) as story_complexity_index,
+
+    /* =========================================
+       CONTRIBUTION METRICS
+    ========================================= */
+
+    coalesce(am.total_artists, 0)
+        as total_artists,
+
+    (
+        coalesce(am.total_artists, 0)
+        +
+        coalesce(cm.total_characters_present, 0)
+    ) as total_contributions,
 
     i.loaded_at,
 
     i.dbt_valid_from as record_valid_from,
+
     i.dbt_updated_at as last_version_update
 
 from current_issues i
@@ -87,5 +163,8 @@ left join {{ ref('stg_series') }} s
 left join {{ ref('stg_formats') }} f
     on i.format_id = f.format_id
 
-left join issue_multiversal_metrics m
-    on i.issue_id = m.issue_id
+left join issue_character_metrics cm
+    on i.issue_id = cm.issue_id
+
+left join issue_artist_metrics am
+    on i.issue_id = am.issue_id
